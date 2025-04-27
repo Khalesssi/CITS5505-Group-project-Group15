73649be8d62bf962df91a976ac5b83320d90cd03
Hannah-X0206 <xwy20020206@gmail.com>
2025-04-27 20:51:05 +0800

    fix: clean venv files, update .gitignore, update project files

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__,
-            template_folder="templates",
-            static_folder="static")
+app = Flask(__name__)
 
-app.secret_key = "your-secret-key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
-db = SQLAlchemy(app)
-
-from app import routes  
+from app import routes
--- app/routes.py
+++ app/routes.py
@@ -4,39 +4,3 @@ from flask import render_template
 @app.route("/")
 def home():
     return render_template("home.html")
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-@app.route("/signup")
-def signup():
-    return render_template("signup.html")
-
-@app.route("/physio")
-def physio_dashboard():
-    return render_template("physio_dashboard.html")
-
-@app.route("/ot")
-def ot_dashboard():
-    return render_template("ot_dashboard.html")
-
-@app.route("/psych")
-def psych_dashboard():
-    return render_template("psych_dashboard.html")
-
-@app.route("/admin")
-def admin_dashboard():
-    return render_template("admin.html")
-
-@app.route("/guardian")
-def guardian_dashboard():
-    return render_template("guardian.html")
-
-@app.route("/supervisor")
-def supervisor_dashboard():
-    return render_template("supervisor.html")
-
-@app.route("/support_worker")
-def support_worker_dashboard():
-    return render_template("support_worker.html")
